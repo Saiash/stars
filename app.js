18d5967230e095eb5ce925ef9e30b16e0a8a6a90
Glitch (saiash-syndicate-bot) <none>
2018-11-30 19:19:26 +0000

    🏭🍱 Checkpoint ./app.js:2426602/330

--- app.js
+++ app.js
@@ -22,3 +22,15 @@ client.on("message", message => {
     }
 })
 client.login("NTE3NjU0ODMxNTkxMDYzNTYz.DuL5IA.NKA6zzIUAQN7UmBYEJl4fna4fCs");
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
