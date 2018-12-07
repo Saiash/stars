9f7afd38dc708891b0bd2d5908af7f98a550fa0e
Glitch (saiash-syndicate-bot-git-git) <none>
2018-12-07 14:20:53 +0000

    🚄🏚 Checkpoint ./app.js:2426602/3048

--- app.js
+++ app.js
@@ -23,28 +23,44 @@ client.on("message", message => {
            text = "команды: !строительство";
         }
         if (message.content == '!строительство') {
-            text = "Для рассчета стоимости постройки сообщение должно быть следующего формата следующего формата: \r\n**!строительство /технология/количество/модификатор/тип**";
+            text = "Для рассчета стоимости постройки сообщение должно быть следующего формата: \r\n**!строительство /технология/количество/модификатор/тип**";
         } else if (message.content.indexOf('!строительство') != -1) {
-            var price = 0;
             var tech = message.content.split('/')[1]*1;
             var count = message.content.split('/')[2]*1;
             var mod = message.content.split('/')[3]*1;
             var type = message.content.split('/')[4]*1;
-            if (type == 1) {
-                price = Math.round((2+2.7*(tech+0.2))*Math.pow(count,1.2)*((20)/Math.pow(23,2.5))/((20- mod)/Math.pow(23-mod,2.5)));
-                var income = Math.round(Math.pow((count+tech)*(0.7+mod/14)+1,1.6+(tech)/8))
-            } else if (type == 2) {
-                price = Math.round((1+9*tech)*Math.pow(count,1.2)*((20)/Math.pow(23,2.5))/((20-mod)/Math.pow(23-mod,2.5)))
-            }
+            var price = pricecalc(tech,count,mod,type);
+            var income = Math.round(Math.pow((count+tech)*(0.7+mod/14)+1,1.6+(tech)/8));
             text = "Цена постройки: **"+price+"**";
-            if (type == 1) {
+            if (income >= 1) {
                 text += "\r\nДоход: **"+ income+"**";
             }
         }
+        if (message.content == '!улучшение') {
+            text = "Для рассчета стоимости улучшения сообщение должно быть следующего формата: \r\n**!улучшение /технология:количество-/технология:количество-/модификатор/тип**";
+        } else if (message.content.indexOf('!улучшение') != -1) {
+          
+        }
+      
         if (text != '') {
             message.channel.send(text);
         }
     }
 })
 
-var price = function (tech,) 
+var pricecalc = function (tech,count,mod,type) {
+  var price = 0;
+    if (type == 1) {
+        price = Math.round((2+2.7*(tech+0.2))*Math.pow(count,1.2)*((20)/Math.pow(23,2.5))/((20- mod)/Math.pow(23-mod,2.5)));
+    } else {
+        price = Math.round((1+9*tech)*Math.pow(count,1.2)*((20)/Math.pow(23,2.5))/((20-mod)/Math.pow(23-mod,2.5)));
+    }
+    return price;
+} 
+var incomecalc = function (tech,count,mod,type) {
+    var income = 0;
+    if (type == 1) {
+      income = Math.round(Math.pow((count+tech)*(0.7+mod/14)+1,1.6+(tech)/8));
+    }
+    return income;
+}
