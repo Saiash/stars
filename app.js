f458f2a5b0cbba52c993453ce458cdb6e031151a
Glitch (saiash-syndicate-bot-git-git) <none>
2019-05-15 08:37:29 +0000

    🎣🎓 Checkpoint ./app.js:2426602/969 ./actioncards.txt:2426602/20902 ./law.txt:2426602/9453

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ game.actions = JSON.parse(fs.readFileSync('actions.txt','utf8'));
 game.planettokens = fs.readFileSync('planets.txt','utf8').split('\n');
 game.spacetokens = fs.readFileSync('space.txt','utf8').split('\n');
 game.actioncards = fs.readFileSync('actioncards.txt','utf8').split('!@#"\n');
+game.lawcards = fs.readFileSync('law.txt','utf8').split('!@#"\n');
 
 
 client.on("message", message => {
@@ -81,7 +82,23 @@ client.on("message", message => {
           var rand = Math.floor(Math.random() * count-1);
           text = game.actioncards[rand];
           game.actioncards.splice(rand,1);
-          fs.writeFile("actioncards.txt", JSON.stringify(game.actioncards.join('!@#"\n')), function(err) {
+          fs.writeFile("actioncards.txt", game.actioncards.join('!@#"\n'), function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+              console.log("The file was saved!");
+          }); 
+        }
+        if (message.content == '!взять закон') {
+          if (message.channel.name != "техническая") {
+            client.guilds.get('577853550517026816').channels.get('578119273642197018').send(message.channel.name+" взяли закон");
+          }
+          var count = game.lawcards.length;
+          console.log(count);
+          var rand = Math.floor(Math.random() * count-1);
+          text = game.lawcards[rand];
+          game.lawcards.splice(rand,1);
+          fs.writeFile("law.txt", game.lawcards.join('!@#"\n'), function(err) {
               if(err) {
                   return console.log(err);
               }
