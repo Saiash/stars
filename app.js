5664395b7de1119b664492700e668cb5d62c009e
Glitch (saiash-syndicate-bot-git-git) <none>
2019-05-15 10:27:30 +0000

    😚📯 Checkpoint ./app.js:2426602/1520

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ game.lawcards = fs.readFileSync('law.txt','utf8').split('!@#"\n');
 
 game.cards = {};
 game.cards = JSON.parse(fs.readFileSync('cards.txt','utf8'));
-
+game.laws = {};
+game.laws = JSON.parse(fs.readFileSync('laws.txt','utf8'));
 
 
 client.on("message", message => {
@@ -87,13 +88,13 @@ client.on("message", message => {
       
         if (message.content == '!взять кд') {
           if (message.channel.name != "техническая") {
-            //client.guilds.get('577853550517026816').channels.get('578119273642197018').send(message.channel.name+" взяли карту действий");
+            client.guilds.get('577853550517026816').channels.get('578119273642197018').send(message.channel.name+" взяли карту действий");
           }
           var count = game.actioncards.length;
           console.log(count);
           var rand = Math.floor(Math.random() * count-1);
           text = game.actioncards[rand];
-          //game.actioncards.splice(rand,1);
+          game.actioncards.splice(rand,1);
           fs.writeFile("actioncards.txt", game.actioncards.join('!@#"\n'), function(err) {
               if(err) {
                   return console.log(err);
@@ -123,7 +124,7 @@ client.on("message", message => {
       
       
         if (message.content.indexOf('!сбросить кд') != -1) {
-          var text = "";
+          var text = "карта сброшена";
           var num = message.content.match(/(\d+)/ig)[0];
           game.cards[message.channel.name].splice((num-1),1);
           fs.writeFile("cards.txt", JSON.stringify(game.cards), function(err) {
@@ -150,7 +151,40 @@ client.on("message", message => {
               }
               console.log("The file was saved!");
           }); 
+          if (game.laws[message.channel.name] == undefined) {
+            game.laws[message.channel.name] = [];
+          }
+          console.log(game.laws[message.channel.name]);
+          game.laws[message.channel.name].push(text);
+          fs.writeFile("laws.txt", JSON.stringify(game.laws), function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+              console.log("The file was saved!");
+            });
         }
+      
+      
+      if (message.content == '!посмотреть законы') {
+          var text = "";
+          Object.keys(game.laws[message.channel.name]).map(function(name, index) {
+                text +=(index+1)+". "+game.laws[message.channel.name][name]+"\n\n";
+            });
+        }
+      
+      
+        if (message.content.indexOf('!сбросить закон') != -1) {
+          var text = "карта сброшена";
+          var num = message.content.match(/(\d+)/ig)[0];
+          game.laws[message.channel.name].splice((num-1),1);
+          fs.writeFile("laws.txt", JSON.stringify(game.laws), function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+              console.log("The file was saved!");
+            });
+        }
+      
         
         if (text != '') {
             text = text.match(/(.|\n){1,1900}/g);
