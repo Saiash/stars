485889087afdb0ddf2bfad5eb637900f50c4c2c9
Glitch (saiash-syndicate-bot-git-git) <none>
2019-05-25 18:10:11 +0000

    🐚🚋 Checkpoint ./app.js:2426602/1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
-app.listen(process.env.PORT);
+app.listen(process.env.PORT); 
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 220000);  
