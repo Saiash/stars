401186f8e5bb9414cd1d1892a2feb69fac076431
Glitch (saiash-syndicate-bot-git-git) <none>
2019-06-03 11:32:19 +0000

    👫😀 Checkpoint ./app.js:2426602/356

--- app.js
+++ app.js
@@ -207,7 +207,6 @@ client.on("message", message => {
           if (game.cards[message.channel.name] == undefined) {
             game.cards[message.channel.name] = [];
           }
-          console.log(game.cards[message.channel.name]);
           game.cards[message.channel.name].push(text);
           fs.writeFile("cards.txt", JSON.stringify(game.cards), function(err) {
               if(err) {
@@ -258,11 +257,9 @@ client.on("message", message => {
           if (message.channel.name != "техническая") {
             client.guilds.get('583729759851249681').channels.get('585045600588922903').send(message.channel.name+" взяли закон");
           }
-          console.log(game.lawcards);
           var count = game.lawcards.length;
           var rand = Math.floor(Math.random() * count-1);
           text = game.lawcards[rand];
-          console.log(text);
           //game.lawcards.splice(rand,1);
           fs.writeFile("law.txt", game.lawcards.join('!@#"\n'), function(err) {
               if(err) {
@@ -273,7 +270,6 @@ client.on("message", message => {
           if (game.laws[message.channel.name] == undefined) {
             game.laws[message.channel.name] = [];
           }
-          console.log(game.laws[message.channel.name]);
           game.laws[message.channel.name].push(text);
           fs.writeFile("laws.txt", JSON.stringify(game.laws), function(err) {
               if(err) {
@@ -288,7 +284,6 @@ client.on("message", message => {
             client.guilds.get('583729759851249681').channels.get('585045600588922903').send(message.channel.name+" взяли секретное задание");
           }
           var count = game.questscards.length;
-          console.log(count);
           var rand = Math.floor(Math.random() * count-1);
           text = game.questscards[rand];
           game.questscards.splice(rand,1);
@@ -301,7 +296,6 @@ client.on("message", message => {
           if (game.quests[message.channel.name] == undefined) {
             game.quests[message.channel.name] = [];
           }
-          console.log(game.quests[message.channel.name]);
           game.quests[message.channel.name].push(text);
           fs.writeFile("quests.txt", JSON.stringify(game.quests), function(err) {
               if(err) {
