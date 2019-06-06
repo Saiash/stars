78f68e6dd6cd62af95a09ed6e88e8dfd3b2db7fa
Glitch (stars-game) <none>
2019-06-06 04:29:09 +0000

    🕋🌦 Checkpoint ./app.js:2426602/19

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ setInterval(() => {
 var game = {};
 game.actions = {};
 game.actions = JSON.parse(fs.readFileSync('actions.txt','utf8'));
+console.log(game);
 
 game.diplomacy = {};
 game.diplomacy = JSON.parse(fs.readFileSync('diplomacy.txt','utf8'));
