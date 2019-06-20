527c6f9afe452bc0827a0ce2c0d4307783374651
Glitch (stars-game) <none>
2019-06-20 06:08:08 +0000

    🕋🌽 Checkpoint ./app.js:2426602/2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);   
-});
+});  
 app.listen(process.env.PORT);  
 setInterval(() => { 
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
